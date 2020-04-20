0368717baaa66a99e76b97f8364b73ab04b90f6f
gudmunduro <goh00@protonmail.com>
2020-04-20 18:00:27 +0000

    Fix reading from file with newline

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -4,6 +4,11 @@ use std::fs;
 pub mod constants;
 
 pub fn read_to_i32(path: &str) -> Result<i32, Box<dyn Error>> {
-    let value = fs::read_to_string(path)?;
+    let mut value = fs::read_to_string(path)?;
+
+    if value.ends_with("\\n") {
+        value = String::from(&value[..(value.len() - 1)]);
+    }
+
     Ok(value.parse::<i32>()?)
 }
--- src/tests.rs
+++ src/tests.rs
@@ -4,4 +4,9 @@ mod tests {
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
+
+    #[test]
+    fn cpu_temp_test() {
+        assert_eq!(cpu::cpu_temp(), Some(value));
+    }
 }
