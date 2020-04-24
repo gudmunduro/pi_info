b8ab9c86c5af4322d77596c35563e22e75ec3ec6
gudmunduro <goh00@protonmail.com>
2020-04-24 00:43:20 +0000

    Add cpu clock speed

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -3,6 +3,11 @@ use crate::common;
 use crate::common::constants;
 
 pub fn cpu_temp() -> Result<f32, Box<dyn Error>> {
-    let res = common::read_to_i32(constants::CPU_TEMP_PATH)?;
+    let res = common::read_to_i32("/sys/class/thermal/thermal_zone0/temp")?;
+    Ok(res as f32 / 1000_f32)
+}
+
+pub fn cpu_clock(core: i32) -> Result<f32, Box<dyn Error>> {
+    let res = common::read_to_i32(&format!("/sys/devices/system/cpu/cpu{}/cpufreq/cpuinfo_cur_freq", core))?;
     Ok(res as f32 / 1000_f32)
 }
